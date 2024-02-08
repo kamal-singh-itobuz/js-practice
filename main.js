c3b5148604b7c7b3753cf4ccc0aeb8c110e39f01
Itobuz <itobuz@Itobuzs-MacBook-Pro.local>
2024-02-08 14:49:00 +0530

    Todo list done using process.argv

--- main.js
+++ main.js
@@ -3,14 +3,36 @@ const rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout,
 });
-
 let todoList = [];
+// Method - 1
+function fun(){
+    rl.question("Add Title : ", function (t) {
+        rl.question("Add Description : ", function (d) {
+            rl.question("Want to show todo list ?(yes/no) ", function (y) {
+                var work = {
+                    title : t,
+                    description : d,
+                    date : new Date(),
+                };
+                todoList.push(work);
+                if(y === "yes"){
+                    console.log("Your todo List is: ", todoList);
+                    rl.close();
+                    return;
+                }
+                else fun();
+            })
+        });
+    });
+}
+fun();
 
+// Method - 2
 function getUserInput() {
     console.log("Enter Your Choice given below: ");
     console.log("Enter 1 for Adding Task. \nEnter 2 for display todo List. \nEnter 0 for exit.");
 
-    rl.question("Enter a NUmber : ", function (num) {
+    rl.question("Enter a Number : ", function (num) {
         let input = parseInt(num);
         performTask(input);
     });
@@ -37,8 +59,8 @@ function performTask(input) {
             break;
         }
         case 2: {
-            console.log(todoList);
-            getUserInput();
+            console.log("Your pending tasks are - ", todoList);
+            rl.close();
             break;
         }
     }
